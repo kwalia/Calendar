a4cb7bc40a49ca9a399e3fd31bda193c01c9d050
Ubuntu <ubuntu@ip-172-31-28-88.ap-south-1.compute.internal>
2020-07-26 21:38:41 +0000

    Calculator code commit

---
+++ src/main/java/xyz/com/Calculator/App.java
@@ -0,0 +1,18 @@
+package xyz.com.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static double division(int num1,int num2 )
+    {
+        return num1/num2;
+    }
+}
